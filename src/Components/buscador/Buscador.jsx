5e01b8937b78d55ccef9794e94f546fa24a22aee
Juan Cabello <juanelcabello@gmail.com>
2021-08-03 00:46:46 +0200

    Buscador parcialmente terminado

--- src/Components/buscador/Buscador.jsx
+++ src/Components/buscador/Buscador.jsx
@@ -7,7 +7,6 @@ const Buscador = () => {
     <form className="buscador__form" onSubmit={submit}>
       <input
         type="text"
-        className="buscador__input"
         placeholder="Buscar Amigos..."
       />
       <i aria-hidden className="fas fa-search"></i>
